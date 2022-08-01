3178965be554e23a2d71441ca7ce9d14ef8751d5
Yasin Mghazli <yasinbarcelos@hotmail.com>
2022-08-01 13:35:10 -0300

    Test

--- tests/LoA/conftest.py
+++ tests/LoA/conftest.py
@@ -1,8 +1,9 @@
 import pytest
-from brownie import LoAToken , accounts
+from brownie import LoAToken
+from scripts.helpful_scripts import get_account
 
 @pytest.fixture
 def token(LoAToken):
-    LoA = LoAToken.deploy({"from": accounts[0]})    
+    LoA = LoAToken.deploy({"from": get_account(index=0)})    
     return LoA
 
